caa1fdebdc50b08ad8df2a7cfe21f78ea074135f
Wesley Aguiar Lopes <wesley.a.lopes@outlook.com>
2024-04-18 17:45:27 -0300

    Create contribution section

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
         $('.mobile-menu').find('i').toggleClass('fa-xmark')
     });
 });
+
+// copy the pix key to the clipboard
+function copy(){
+    var pixkey = document.getElementById("key-pix");
+    pixkey.select();
+    document.execCommand("copy");
+    alert("Chave Pix copiada!");
+}
+
+// copy the pix key to the clipboard, in mobile version
+function copy_mobile(){
+    var pixkeymobile = document.getElementById("key-pix-mobile");
+    pixkeymobile.select();
+    document.execCommand("copy");
+    alert("Chave Pix copiada!");
+}
