c4ddc5ceda76077d3ed199c289336b0f51df0ad4
Wesley Aguiar Lopes <wesley.a.lopes@outlook.com>
2024-09-06 22:06:32 -0300

    Change the class name to default by removing the _ and replacing it with -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // mobile navigation menu - open and close mobile menu
 
 $(document).ready(function() {
-    $('.mobile-menu').on('click', function() {
+    $('.btn-mobile-menu').on('click', function() {
         $('.mobile-nav-bar').toggleClass('active')
-        $('.mobile-menu').find('i').toggleClass('fa-xmark')
+        $('.btn-mobile-menu').find('i').toggleClass('fa-xmark')
     });
 });
 
