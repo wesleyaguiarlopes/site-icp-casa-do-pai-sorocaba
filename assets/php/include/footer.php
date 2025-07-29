615c04c275ef5f7402a3f571d0738f555ef7defa
Wesley Aguiar Lopes <wesley.a.lopes@outlook.com>
2025-07-29 19:08:55 -0300

    Changes the copyright year annually. Changes the code of the button that accesses the admin page

--- assets/php/include/footer.php
+++ assets/php/include/footer.php
@@ -1,5 +1,14 @@
 <section class="content">
-    <p>Copyright © 2024 <span>ICP Casa do Pai</span> Sorocaba.</p>
+    <p>Copyright © <span id="current-year"></span> <span id="church-name">ICP Casa do Pai</span> Sorocaba.</p>
     <p>Todos os direitos reservados.</p>
-    <a class="btn-admin-access" href="admin" title="Acesso ao gerenciamento do site">Acesso Admin</a>
+    <button id="btn_admin-access" title="Acesso ao gerenciamento do site">
+        <i class="fa-solid fa-user-tie"></i> Acesso Admin
+    </button>
 </section>
+<script>
+    document.getElementById("current-year").innerText = new Date().getFullYear();
+
+    document.getElementById("btn_admin-access").addEventListener("click", function() {
+        location.href="admin";
+    });
+</script>
